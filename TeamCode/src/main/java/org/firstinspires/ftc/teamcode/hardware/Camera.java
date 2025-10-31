75fa4776119103259c92afef2fc8c76f5d3f62e9
Kolton Bloom <bloomkd46@icloud.com>
2025-10-31 14:59:32 +0000

    Highlighted important telemetries

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Camera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Camera.java
@@ -144,12 +144,12 @@ public class Camera {
         if (detection.metadata != null) {
           telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
           telemetry.addLine(String.format("Location: %s", detection.position.toString()));
-          telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y,
+          telemetry.addLine(String.format("XYZ >>%6.1f<< %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y,
               detection.ftcPose.z));
           telemetry
-              .addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll,
+              .addLine(String.format("PRY %6.1f %6.1f >%6.1f<  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll,
                   detection.ftcPose.yaw));
-          telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range,
+          telemetry.addLine(String.format("RBE >>%6.1f<< %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range,
               detection.ftcPose.bearing, detection.ftcPose.elevation));
         } else {
           telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
