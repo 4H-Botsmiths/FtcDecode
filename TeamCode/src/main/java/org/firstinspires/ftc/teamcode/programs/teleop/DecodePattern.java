311241560f36911d5f3707f7c88336b4fa6a629e
Kolton Bloom <bloomkd46@icloud.com>
2025-11-30 00:25:45 +0000

    intake always runs now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/programs/teleop/DecodePattern.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/programs/teleop/DecodePattern.java
@@ -319,6 +319,7 @@ public class DecodePattern extends OpMode {
     robot.drive(x, y, r);
 
     if (gamepad1.right_trigger > 0.5) {
+      robot.intake.setPowerAll(1.0);
       //-----------------------------------------Indexer-----------------------------------------
       int patternIndex = classifiedArtifacts % 3;
       Indexer.BallColor desiredColor = classifiedArtifacts < 9 ? obeliskMotif.getPattern()[patternIndex]
